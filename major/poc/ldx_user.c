696bc58dcda79b36b43e8883bc23e6217ac43bf4
Milo-D <David.Milosevic@web.de>
2019-09-09 22:09:18 +0200

    Minor Changes

--- major/poc/ldx_user.c
+++ major/poc/ldx_user.c
@@ -24,80 +24,77 @@ ssize_t write_char_dev(char*);
 
 int main(int argc, char** argv) {
 
-	if(argc < 2) {
+    if(argc < 2) {
 
-		printf("Missing arguments...\n");
-		return EXIT_FAILURE;
-	}
+        printf("Missing arguments...\n");
+        return EXIT_FAILURE;
+    }
 
-	if(argc < 3 && strcmp(argv[1], "-w") == 0) {
+    if(argc < 3 && strcmp(argv[1], "-w") == 0) {
 
-		printf("Please specify Data to write to Char Device...\n");
-		return EXIT_FAILURE;
-	}
+        printf("Please specify Data to write to Char Device...\n");
+        return EXIT_FAILURE;
+    }
 
-	if(strcmp(argv[1], "-r") == 0) {
+    if(strcmp(argv[1], "-r") == 0) {
 
-		printf("Total bytes read: %ld\n", read_char_dev());
-		return EXIT_SUCCESS;
-	}
+        printf("Total bytes read: %ld\n", read_char_dev());
+        return EXIT_SUCCESS;
+    }
 
-	if(strcmp(argv[1], "-w") == 0) {
+    if(strcmp(argv[1], "-w") == 0) {
 	
-		printf("Total bytes written: %ld\n", write_char_dev( argv[2] ));
-		return EXIT_SUCCESS;		
-	}
+        printf("Total bytes written: %ld\n", write_char_dev( argv[2] ));
+        return EXIT_SUCCESS;		
+    }
 
-	printf("Unknown Option: %s\n", argv[1]);
+    printf("Unknown Option: %s\n", argv[1]);
 	
-	return EXIT_FAILURE;
+    return EXIT_FAILURE;
 }
 
 ssize_t read_char_dev(void) {
 
-	int device_fd = open(DEVICE_FILE, O_RDONLY);
+    int device_fd = open(DEVICE_FILE, O_RDONLY);
 
-	if(device_fd < 0) {
+    if(device_fd < 0) {
 
-		printf("An error occured: %s,\tLDX Device returned: %d\n", strerror(errno), device_fd);
-		exit( EXIT_FAILURE );
-	}
+        printf("An error occured: %s,\tLDX Device returned: %d\n", strerror(errno), device_fd);
+        exit( EXIT_FAILURE );
+    }
 
-	char* dev_buffer = (char*) malloc(sizeof(char) * (MAX_DATA_BUFFER + 1));
+    char* dev_buffer = (char*) malloc(sizeof(char) * (MAX_DATA_BUFFER + 1));
 
-	ssize_t total_bytes = read(device_fd, (void*) dev_buffer, MAX_DATA_BUFFER);
-	dev_buffer[ total_bytes ] = '\0';
+    ssize_t total_bytes = read(device_fd, (void*) dev_buffer, MAX_DATA_BUFFER);
+    dev_buffer[ total_bytes ] = '\0';
 
-	printf("String read from LDX Device: %s\n", dev_buffer);
+    printf("String read from LDX Device: %s\n", dev_buffer);
 
-	free(dev_buffer);
-	close(device_fd);
+    free(dev_buffer);
+    close(device_fd);
 
-	return total_bytes;
+    return total_bytes;
 }
 
 ssize_t write_char_dev(char* data) {
 
-	int device_fd = open(DEVICE_FILE, O_WRONLY);
+    int device_fd = open(DEVICE_FILE, O_WRONLY);
 
-	if(device_fd < 0) {
+    if(device_fd < 0) {
 
-		printf("An error occured: %s,\tLDX Device returned: %d\n", strerror(errno), device_fd);
-		exit( EXIT_FAILURE );
-	}
+        printf("An error occured: %s,\tLDX Device returned: %d\n", strerror(errno), device_fd);
+        exit( EXIT_FAILURE );
+    }
 
-	ssize_t total_bytes = write(device_fd, (const void*) data, strlen(data));
+    ssize_t total_bytes = write(device_fd, (const void*) data, strlen(data));
 
-	if(total_bytes < 0) {
+    if(total_bytes < 0) {
 
-		printf("An error occured: Could not write to LDX,\tLDX Device returned: %ld\n", total_bytes);
-		exit( EXIT_FAILURE );
-	}
+        printf("An error occured: Could not write to LDX,\tLDX Device returned: %ld\n", total_bytes);
+        exit( EXIT_FAILURE );
+    }
 
-	close(device_fd);
+    close(device_fd);
 
-	return total_bytes;
+    return total_bytes;
 }
-
-
-
